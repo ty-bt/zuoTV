9a990180e18ee54eecf4d1133016d284a1f36016
谭勇 <ty_bt@live.cn>
2017-03-09 19:21:08 +0800

    定时任务改为7分钟  禁止多个执行

--- grails-app/jobs/com/mtv/jobs/RoomJob.groovy
+++ grails-app/jobs/com/mtv/jobs/RoomJob.groovy
@@ -1,5 +1,6 @@
 package com.mtv.jobs
 
+import com.mtv.Room
 import grails.util.Environment
 
 
@@ -10,12 +11,16 @@ class RoomJob {
     def platformService
     def recommendService
 
+    // 禁止同时出现两个 顺序执行
+    def concurrent = false
+
     static triggers = {
         // 10分钟刷新一次
-        simple repeatInterval: 600000l, startDelay: 10000 // execute job once in 5 seconds
+        simple repeatInterval: 60000L * 7, startDelay: 10000 // execute job once in 5 seconds
     }
 
     def execute() {
+        Room.get(1L).delete()
         Long start = System.currentTimeMillis()
         println "定时任务执行 " + new Date()
         // 只有生产环境开启
