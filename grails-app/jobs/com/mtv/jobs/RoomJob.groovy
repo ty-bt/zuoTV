3c643f25b4d94d763beb57bb0996ffc065c5813e
白糖 <ty_be@live.cn>
2017-07-11 21:33:54 +0800

    定时任务所有环境下都执行

--- grails-app/jobs/com/mtv/jobs/RoomJob.groovy
+++ grails-app/jobs/com/mtv/jobs/RoomJob.groovy
@@ -23,12 +23,12 @@ class RoomJob {
         Long start = System.currentTimeMillis()
         println "定时任务执行 " + new Date()
         // 只有生产环境开启
-        if(Environment.current == Environment.PRODUCTION){
+//        if(Environment.current == Environment.PRODUCTION){
             roomService.loadAllT()
             typeService.reloadAll()
             platformService.statistics()
             recommendService.reload()
-        }
+//        }
         println "定时任务执行完成,用时" + (System.currentTimeMillis() - start) + "ms"
 
     }
